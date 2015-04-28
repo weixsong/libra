ae228396aff498f212d6cb6fc338276ea84d7419
weixsong <watkinsong@163.com>
2015-04-28 16:44:56 +0800

    clean up

--- OCRDemo/src/com/example/homework/MainActivity.java
+++ OCRDemo/src/com/example/homework/MainActivity.java
@@ -25,7 +25,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private static final String TAG = "MainActivity";
