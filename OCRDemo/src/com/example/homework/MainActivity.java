df6019df1176168c0f0bb52af5016152345108e3
weixsong <watkinsong@163.com>
2015-04-28 16:25:16 +0800

    add OCRDemo

---
+++ OCRDemo/src/com/example/homework/MainActivity.java
@@ -0,0 +1,217 @@
+package com.example.homework;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private static final String TAG = "MainActivity";
+
+	private static final int REQUEST_IMAGE_CAPTURE = 1;
+	private String lang = "chi_sim";
+
+	private ImageView iv;
+
+	public static final String ALBUM = "OCR";
+	public static final String DATA_PATH = Environment
+			.getExternalStorageDirectory().toString() + "/OCR/";
+	public static final String OCR_CONTENT = "OCR_CONTENT";
+	public static final String IMAGE = MainActivity.DATA_PATH + "/ocr.jpg";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+		iv = (ImageView) findViewById(R.id.imageView1);
+
+		// check and copy files
+		checkAndCopyFiles();
+
+		iv.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				dispatchTakePictureIntent();
+			}
+		});
+	}
+
+	private void checkAndCopyFiles() {
+		String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+		for (String path : paths) {
+			File dir = new File(path);
+			if (!dir.exists()) {
+				if (!dir.mkdirs()) {
+					Log.v(TAG, "ERROR: Creation of directory " + path
+							+ " on sdcard failed");
+					return;
+				} else {
+					Log.v(TAG, "Created directory " + path + " on sdcard");
+				}
+			}
+		}
+
+		String traineddata_path = DATA_PATH + "tessdata/" + lang
+				+ ".traineddata";
+		if (!(new File(traineddata_path)).exists()) {
+			try {
+				AssetManager assetManager = getAssets();
+				InputStream in = assetManager.open("tessdata/" + lang
+						+ ".traineddata");
+				OutputStream os = new FileOutputStream(DATA_PATH + "tessdata/"
+						+ lang + ".traineddata");
+
+				// Transfer bytes from in to out
+				byte[] buf = new byte[1024];
+				int len;
+				while ((len = in.read(buf)) > 0) {
+					os.write(buf, 0, len);
+				}
+				in.close();
+				os.close();
+
+				Log.v(TAG, "Copied " + lang + " traineddata");
+			} catch (IOException e) {
+				Log.e(TAG,
+						"Was unable to copy " + lang + " traineddata "
+								+ e.toString());
+			}
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.open_camera) {
+			dispatchTakePictureIntent();
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void dispatchTakePictureIntent() {
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		// Ensure that there's a camera activity to handle the intent
+		if (intent.resolveActivity(getPackageManager()) != null) {
+			// Create the File where the photo should go
+			File photoFile = new File(IMAGE);
+			// Continue only if the File was successfully created
+			if (photoFile != null) {
+				intent.putExtra(MediaStore.EXTRA_OUTPUT,
+						Uri.fromFile(photoFile));
+				startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+			}
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Log.i(TAG, "onActivityResult");
+		if (resultCode == RESULT_OK
+				&& requestCode == MainActivity.REQUEST_IMAGE_CAPTURE) {
+			 ocr_process();
+		} else {
+			Log.v(TAG, "User cancelled");
+		}
+	}
+
+	private void ocr_process() {
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 4;
+		Bitmap bitmap = BitmapFactory.decodeFile(IMAGE, options);
+
+		try {
+			ExifInterface exif = new ExifInterface(IMAGE);
+			int exifOrientation = exif.getAttributeInt(
+					ExifInterface.TAG_ORIENTATION,
+					ExifInterface.ORIENTATION_NORMAL);
+
+			Log.v(TAG, "Orient: " + exifOrientation);
+
+			int rotate = 0;
+			switch (exifOrientation) {
+			case ExifInterface.ORIENTATION_ROTATE_90:
+				rotate = 90;
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_180:
+				rotate = 180;
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_270:
+				rotate = 270;
+				break;
+			}
+
+			Log.v(TAG, "Rotation: " + rotate);
+
+			if (rotate != 0) {
+				// Getting width & height of the given image.
+				int w = bitmap.getWidth();
+				int h = bitmap.getHeight();
+
+				// Setting pre rotate
+				Matrix mtx = new Matrix();
+				mtx.preRotate(rotate);
+
+				// Rotating Bitmap
+				bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+			}
+
+			// Convert to ARGB_8888, required by tess
+			bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+		} catch (IOException e) {
+			Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+		}
+
+		Log.v(TAG, "BaseApi initializing...");
+		TessBaseAPI baseApi = new TessBaseAPI();
+		baseApi.setDebug(true);
+		baseApi.init(DATA_PATH, lang);
+		baseApi.setPageSegMode(TessBaseAPI.PageSegMode.PSM_AUTO_ONLY);
+		baseApi.setImage(bitmap);
+		String recognizedText = baseApi.getUTF8Text();
+		baseApi.end();
+
+		recognizedText = recognizedText.trim();
+
+		Intent intent = new Intent(this, ResultViewer.class);
+		intent.putExtra(OCR_CONTENT, recognizedText);
+		this.startActivity(intent);
+	}
+}
---
+++ OCRDemo/src/com/example/homework/ResultViewer.java
@@ -0,0 +1,70 @@
+package com.example.homework;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ResultViewer extends Activity {
+	
+	private static final String TAG = "ResultViewer";
+	
+	private TextView tv;
+	private ImageView iv;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.result_view);
+		tv = (TextView) findViewById(R.id.ocr_results);
+		iv = (ImageView) findViewById(R.id.cropped_image);
+		
+		String msg = this.getIntent().getStringExtra(MainActivity.OCR_CONTENT);
+		tv.setText(msg);
+		
+		new AsyncImageLoader().execute(MainActivity.IMAGE);
+	}
+
+	private class AsyncImageLoader extends AsyncTask<String, Integer, Bitmap> {
+		private final String TAG = "com.example.homework.AsyncLoadImages";
+
+		private final int targetW = 500;
+		private final int targetH = 500;
+
+		@Override
+		protected Bitmap doInBackground(String... arg0) {
+			Log.i(TAG, "doInBackgroud");
+			Bitmap b = readImage(arg0[0]);
+			return b;
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap bitmap) {
+			Log.i(TAG, "onPostExecute");
+			iv.setImageBitmap(bitmap);
+		}
+
+		private Bitmap readImage(String file) {
+			BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+			bmOptions.inJustDecodeBounds = true;
+			BitmapFactory.decodeFile(file, bmOptions);
+			int photoW = bmOptions.outWidth;
+			int photoH = bmOptions.outHeight;
+
+			// Determine how much to scale down the image
+			int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+			// Decode the image file into a Bitmap sized to fill the View
+			bmOptions.inJustDecodeBounds = false;
+			bmOptions.inSampleSize = scaleFactor;
+			bmOptions.inPurgeable = true;
+
+			Bitmap bitmap = BitmapFactory.decodeFile(file, bmOptions);
+			return bitmap;
+		}
+	}
+}
