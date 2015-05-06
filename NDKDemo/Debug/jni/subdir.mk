4264932c829bc544d2f1f934f37101501d9e0023
weixsong <watkinsong@163.com>
2015-05-06 22:57:51 +0800

    update

--- NDKDemo/Debug/jni/subdir.mk
+++ NDKDemo/Debug/jni/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 jni/%.o: ../jni/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/home/songwei/Softwares/OpenCV-2.4.10-android-sdk/sdk/native/jni/include -I"/home/songwei/Softwares/android-ndk-r10d/platforms/android-21/arch-arm/usr/include" -I"/home/songwei/Softwares/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.9/include" -I"/home/songwei/Softwares/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi-v7a/include/bits" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I"/home/wesong/software/android-ndk-r10d/platforms/android-21/arch-arm/usr/include" -I"/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.9/include" -I"/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.9/libs/armeabi-v7a/include/bits" -I/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
