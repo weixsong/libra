1430cbeeceab82d6ef538b0a54d90b0d7b95b5d3
weixsong <watkinsong@163.com>
2015-05-06 23:05:28 +0800

    update

--- NDKDemo/jni/Android.mk
+++ NDKDemo/jni/Android.mk
@@ -1,12 +1,12 @@
-LOCAL_PATH := $(call my-dir)  
-include $(CLEAR_VARS)  
+LOCAL_PATH := $(call my-dir) 
+include $(CLEAR_VARS)
 OPENCV_LIB_TYPE:=STATIC
 OPENCV_INSTALL_MODULES:=on
 OPENCV_CAMERA_MODULES:=off
 ifeq ("$(wildcard $(OPENCV_MK_PATH))","")
-#try to load OpenCV.mk from default install location  
+#try to load OpenCV.mk from default install location
 include /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/OpenCV.mk
-else  
+else
 include $(OPENCV_MK_PATH)
 endif
 LOCAL_MODULE    := native
