12058d7191e8bdca215976e27f5b22dd419f3798
weixsong <watkinsong@163.com>
2015-05-08 15:36:18 +0800

    update

---
+++ ASMDemo/obj/local/armeabi/objs/Native/Native.o.d
@@ -0,0 +1,203 @@
+/home/wesong/wesong/pisces/libra/ASMDemo/obj/local/armeabi/objs/Native/Native.o: \
+ /home/wesong/wesong/pisces/libra/ASMDemo/jni/Native.cpp \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/jni.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/string.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/strings.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/memory.h \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/log.h \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/opencv.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/assert.h \
+ /home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/math.h \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/limits \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/config.h \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/photo/photo.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/photo/photo_c.h \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/video/video.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/video/tracking.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/video/background_segm.hpp \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/list \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/calib3d/calib3d.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/affine.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/ml/ml.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/contrib/contrib.hpp \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/contrib/retina.hpp \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/valarray \
+ /home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/contrib/openfabmap.hpp \
+ /home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/set \
+ /home/wesong/wesong/pisces/libra/ASMDemo/jni/./stasm/stasm_lib.h
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/string.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/memory.h:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/log.h:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/opencv.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/assert.h:
+
+/home/wesong/software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/math.h:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/limits:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/config.h:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/photo/photo.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/photo/photo_c.h:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/video/video.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/video/tracking.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/video/background_segm.hpp:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/list:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/calib3d/calib3d.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/affine.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/ml/ml.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/contrib/contrib.hpp:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/contrib/retina.hpp:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/valarray:
+
+/home/wesong/software/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/contrib/openfabmap.hpp:
+
+/home/wesong/software/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/set:
+
+/home/wesong/wesong/pisces/libra/ASMDemo/jni/./stasm/stasm_lib.h:
